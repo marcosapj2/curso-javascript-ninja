478ba7349a7c4118483eff4ca4db89b605561da9
Marcos <marcosapj2@hotmail.com>
2018-11-19 11:35:55 -0200

    Fazendo o exercício 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,17 +3,19 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
-
+var qualquer = ['Marcos', 'Pedro', 'Larissa', 'João', 'Maria', true, false, true, false, 1, 2, 3, 4, 5, 6,];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
-
+function inArray (a) {
+    return a;
+}
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-
+console.log(inArray(qualquer)[1]);
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -21,18 +23,23 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
-
+function naoSei (a, b) {
+    return console.log(a[b]);
+}
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
-
+var different = [true, 1, 'string', {propriedade: 'variavel', metodo: function () {} }, function (){}]
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
-
+console.log(naoSei(qualquer, 0));
+console.log(naoSei(qualquer, 1));
+console.log(naoSei(qualquer, 2));
+console.log(naoSei(qualquer, 3));
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -46,10 +53,13 @@ propriedades:
 - A função deve retornar o objeto referente ao livro passado por parâmetro.
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
-*/
+*/  
 // ?
-
-/*
+function book ( livro ) {
+    var object = {livro1: {quantidadePaginas: 100, autor: 'Marcos Aurélio', editora: 'Casa do Código'}, livro2: {quantidadePaginas: 200, autor: 'Marcos Aurélio', editora: 'Casa do Código'}, livro3: {quantidadePaginas: 120, autor: 'Marcos Aurélio', editora: 'Casa do Código'}};
+    return livro;
+    }
+    /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
